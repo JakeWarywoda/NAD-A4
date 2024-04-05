85dbf173993a7f4691704a5dc365fddccbf3e557
JakeWarywoda <jwarywoda7248@conestogac.on.ca>
2024-04-05 06:00:45 -0400

    Practiced AJAX calls

--- index.js
+++ index.js
@@ -21,3 +21,40 @@ document.addEventListener('scroll', ()=>{
     const posY = window.scrollY
     console.log(posY)
 })
+
+
+// GET the data with ajax
+const url = 'https://swapi.dev/api/people/'
+
+// 1. jquery ajax method <- this is what we are going to use in the course
+
+$.ajax({
+    type: 'GET',
+    url: url,
+    success: function(response) {
+        console.log('jquery ajax', response)
+    },
+    error: function(error) {
+        console.log(error)
+    }
+})
+
+// 2. XMLHttpRequest
+const req = new XMLHttpRequest()
+
+req.addEventListener('readystatechange', ()=>{
+    if(req.readyState===4){
+        console.log('xhttp', JSON.parse(req.responseText))
+    }
+})
+
+req.open('GET', url)
+req.send()
+
+// 3. fetch method
+fetch(url)
+.then(resp=> resp.json()).then(data=> console.log('fetch', data))
+.catch(err=> console.log(err))
+
+
+// other popular: axios library, async await + fetch
