ec0cfad52bf52d809938430e00f7dd452fecef0b
JakeWarywoda <jwarywoda7248@conestogac.on.ca>
2024-04-06 03:16:39 -0400

    Finished 'Getting Posts Objects with Ajax'

--- venv/src/posts/static/posts/main.js
+++ venv/src/posts/static/posts/main.js
@@ -2,7 +2,7 @@ console.log('hello world')
 
 
 const helloWorldBox = document.getElementById('hello-world')
-
+const postsBox = document.getElementById('posts-box')
 // helloWorldBox.textContent = 'hello world'
 
 $.ajax({
@@ -14,5 +14,23 @@ $.ajax({
     },
     error: function(error){
         console.log('error', error)
+    }
+})
+
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+    success: function(response){
+        console.log(response)
+        const data = response.data
+        console.log(data)
+        data.forEach(el => {
+            postsBox.innerHTML += `
+                ${el.title} - <b>${el.body}</b><br>
+            `
+        });
+    },
+    error: function(error){
+        console.log(error)
     }
 })
